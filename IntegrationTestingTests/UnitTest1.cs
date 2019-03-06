2de611044569adbc0bc105dc6919bf96c410a77e
davidgouge <davegouge@googlemail.com>
2019-03-06 10:55:24 +0000

    Fixed 404

--- IntegrationTestingTests/UnitTest1.cs
+++ IntegrationTestingTests/UnitTest1.cs
@@ -1,12 +1,19 @@
 using System.Net;
+using System.Reflection;
 using System.Threading.Tasks;
 using FluentAssertions;
 using IntegrationTestingWeb;
+using IntegrationTestingWeb.Controllers;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 
 namespace Tests
@@ -27,7 +34,17 @@ namespace Tests
         [Test]
         public async Task GetPrivacy_Through_Test_Startup_In_Test_Project()
         {
-            var builder = new WebHostBuilder().UseStartup<TestStartupInTestProject>();
+            var builder = new WebHostBuilder().ConfigureServices(services =>
+            {
+                var startupAssembly = typeof(TestStartupInTestProject).GetTypeInfo().Assembly;
+
+                var manager = new ApplicationPartManager();
+                manager.ApplicationParts.Add(new AssemblyPart(startupAssembly));
+                manager.ApplicationParts.Add(new AssemblyPart(typeof(HomeController).Assembly));
+                manager.FeatureProviders.Add(new ControllerFeatureProvider());
+                manager.FeatureProviders.Add(new ViewComponentFeatureProvider());
+                services.AddSingleton(manager);
+            }).UseStartup<TestStartupInTestProject>();
             var client = new TestServer(builder).CreateClient();
 
             var result = await client.GetAsync("/Home/Privacy");
@@ -48,7 +65,7 @@ namespace Tests
     }
     
     public class TestStartupInTestProject : Startup
-    {
+    {   
         public TestStartupInTestProject(IConfiguration configuration) : base(configuration)
         {
         }
--- IntegrationTestingWeb/Startup.cs
+++ IntegrationTestingWeb/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IntegrationTestingWeb.Controllers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -39,7 +40,7 @@ namespace IntegrationTestingWeb
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);//.AddApplicationPart(typeof(HomeController).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
